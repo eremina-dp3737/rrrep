401ba596ef6514f0f59cee2982341f7db4cc7dac
eremina-dp3737 <eremina2oo8@yandex.ru>
2026-09-17 17:48:00 +0500

    инициализация приложения

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/hello', (req, res) => {
+  res.send('hihi');
+});
+app.get('/kl', (req, res) => {
+  res.send('krzrw');
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
